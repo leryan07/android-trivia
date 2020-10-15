1c2af30b4d10786fd72d4a62c83d69d0e13f90ff
Ryan <leryan07@gmail.com>
2020-10-15 15:53:42 -0500

    Show toast when preference has been changed

--- app/src/main/java/com/example/android/navigation/SettingsActivity.kt
+++ app/src/main/java/com/example/android/navigation/SettingsActivity.kt
@@ -1,6 +1,8 @@
 package com.example.android.navigation
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 
@@ -14,9 +16,30 @@ class SettingsActivity : AppCompatActivity() {
                 .commit()
     }
 
-    class SettingsFragment : PreferenceFragmentCompat() {
+    class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
+
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.preferences, rootKey)
+        }
+
+        override fun onResume() {
+            super.onResume()
+            preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+        }
+
+        override fun onPause() {
+            super.onPause()
+            preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+        }
+
+        override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
+            if (key == "numberOfQuestions") {
+                val numQuestions = sharedPreferences.getString("numberOfQuestions", "3");
+
+                Toast.makeText(this.context,
+                        "Number of questions was updated to: $numQuestions", Toast.LENGTH_SHORT)
+                        .show()
+            }
         }
     }
 }
